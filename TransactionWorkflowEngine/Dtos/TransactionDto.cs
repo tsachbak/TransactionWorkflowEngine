e4cb945a9da5564cd2c1f2ebf58023721b56d727
tsach <tsachbak9@gmail.com>
2026-02-18 17:45:48 +0200

    Create TransactionDto and TransactionStatusDto DTO's.

---
+++ TransactionWorkflowEngine/Dtos/TransactionDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TransactionWorkflowEngine.Dtos
+{
+    public sealed class TransactionDto
+    {
+        public Guid Id { get; set; }
+        public TransactionStatusDto CurrentStatus { get; set; } = new TransactionStatusDto();
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ TransactionWorkflowEngine/Dtos/TransactionStatusDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TransactionWorkflowEngine.Dtos
+{
+    public sealed class TransactionStatusDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
